e0a36a68deaf9640edb60420bff77685544537dd
masaharu <dabao456democebu3@gmail.com>
2024-10-01 21:47:11 +0900

    SwiftUIでは不要なDelegate処理を削除した

--- SamplexHomeScreenQuickActions/SamplexHomeScreenQuickActionsApp.swift
+++ SamplexHomeScreenQuickActions/SamplexHomeScreenQuickActionsApp.swift
@@ -10,7 +10,6 @@ import SwiftUI
 @main
 struct SamplexHomeScreenQuickActionsApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate // たぶんSwiftUIで不要
     @Environment(\.scenePhase) var scenePhase
 
     private let quickActionState = QuickActionState.shared
@@ -32,36 +31,5 @@ struct SamplexHomeScreenQuickActionsApp: App {
                 fatalError()
             }
         }
-    }
-}
-
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-
-    private let quickActionState = QuickActionState.shared
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-
-        quickActionState.selectAction(by: options.shortcutItem)
-
-        let configuration = UISceneConfiguration(
-            name: connectingSceneSession.configuration.name,
-            sessionRole: connectingSceneSession.role
-        )
-        configuration.delegateClass = SceneDelegate.self
-        return configuration
-    }
-}
-
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
-
-    private let quickActionState = QuickActionState.shared
-
-    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, options: UIScene.ConnectionOptions, completionHandler: @escaping (Bool) -> Void) {
-        quickActionState.selectAction(by: options.shortcutItem)
-        completionHandler(true)
     }
 }
